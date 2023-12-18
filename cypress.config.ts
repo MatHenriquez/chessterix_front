1c9e37668345f64e1406330439323e6c8ce42b93
MatHenriquez <matias.henriquez.dev@gmail.com>
2023-12-18 20:03:22 -0300

    Configure e2e testing with Cypess

---
+++ cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {}
+  },
+
+  component: {
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack'
+    }
+  }
+});
