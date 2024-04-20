de80b6dbd5a04fb66d6b06f043acb2bbd591d1c8
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-04-19 23:47:10 -0300

    Refactor landing page component in Landing.tsx

--- app/components/landing/Landing.tsx
+++ app/components/landing/Landing.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 
 export default function Landing() {
   return (
-    <section className="relative bg-[url(/image/anime-2.jpg)] bg-cover bg-center bg-no-repeat" data-cy="landing-container">
-      <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex h-screen lg:items-center lg:px-8">
-        <div className="flex flex-col items-center max-w-xl text-center mx-auto mb-44 ltr:sm:text-left rtl:sm:text-right">
-          <Title />
-          <div className='flex flex-col items-center'>
-            <Message />
-          </div>
-          <div className="mt-8 mb-2 lg:mt-60 flex flex-wrap gap-4 text-center">
+    <section
+      className="bg-[url(/image/anime-2.jpg)] bg-cover bg-center bg-no-repeat h-screen"
+      data-cy="landing-container"
+    >
+      <div
+        className="grid grid-rows-12 h-screen justify-items-center max-w-xl text-center mx-auto mb-44 ltr:sm:text-left rtl:sm:text-right"
+        data-cy="landing-content"
+      >
+        <div className="row-span-4 place-content-center">
+          <Message />
+        </div>
+        <div className="grid grid-cols-2 grid-rows-12 gap-x-6">
+          <div className="col-span-1">
             <SignInButton />
+          </div>
+          <div className="col-span-1">
             <SignUpButton />
           </div>
-          <GuestLink />
+          <div className="col-span-2">
+            <GuestLink />
+          </div>
         </div>
       </div>
     </section>
@@ -21,24 +30,27 @@ export default function Landing() {
 }
 
 const Title = () => (
-  <h1 className="text-9xl font-extrabold leading-none sm:text-title font-stranger lg:mt-32 lg:mb-40" data-cy="landing-title">
-    {"Let's play"}
-    <strong className="block font-extrabold text-rose-700">
-      If you dare...
-    </strong>
-  </h1>
+  <h2 className="text-8xl leading-none mb-24 shadow-xl rounded shadow-red-200/25" data-cy="landing-title">
+    <strong className="block font-extrabold text-6xl mt-12">Welcome to</strong>
+  </h2>
 );
 
 const Message = () => (
-  <p className="max-w-lg mt-8 lg:mb-8 lg:mt-0 text-6xl lg:text-8xl font-stranger" data-cy="welcome-message">
-    Welcome to <span className="font-bold text-rose-700">Chessterix</span>
-  </p>
+  <>
+    <Title />
+    <p
+      className="max-w-lg inline-block align-middle h-36 py-2 px-12 rounded-md lg:mb-8 lg:mt-0 text-9xl lg:text-title font-stranger bg-red-100/75 shadow-lg shadow-red-200/75 font-extrabold text-rose-700"
+      data-cy="welcome-message"
+    >
+      Chessterix
+    </p>
+  </>
 );
 
 const SignInButton = () => (
   <a
     href="#"
-    className="block w-full rounded text-sm lg:text-lg bg-red-main px-12 py-3 font-medium text-white shadow hover:bg-kimono-200 focus:outline-none focus:ring active:bg-kimono-700 sm:w-auto"
+    className="block w-full rounded text-sm lg:text-lg bg-red-main px-12 py-3 font-medium text-white shadow-lg hover:bg-kimono-200 focus:outline-none focus:ring active:bg-kimono-700 sm:w-auto shadow-red-50/75"
     data-cy="sign-in-button"
   >
     Sing In
@@ -48,7 +60,7 @@ const SignInButton = () => (
 const SignUpButton = () => (
   <a
     href="#"
-    className="block w-full rounded bg-white px-12 py-3 text-sm lg:text-lg font-medium text-kimono-200 shadow hover:text-red-main hover:bg-bone-500 focus:outline-none focus:ring active:text-kimono-700 sm:w-auto"
+    className="block w-full rounded bg-white px-12 py-3 text-sm lg:text-lg font-medium text-kimono-200 shadow-lg shadow-red-50/75 hover:text-red-main hover:bg-bone-500 focus:outline-none focus:ring active:text-kimono-700 sm:w-auto"
     data-cy="sign-up-button"
   >
     Sing Up
@@ -56,5 +68,11 @@ const SignUpButton = () => (
 );
 
 const GuestLink = () => (
-  <a href="" className='text-rose-700 underline bg-white p-1 rounded border-2 border-bone-500' data-cy="guest-link">Continue as a guest</a>
+  <a
+    href=""
+    className="text-rose-700 underline bg-white p-1 w-64 rounded border-2 border-bone-500 shadow-lg shadow-red-400/75"
+    data-cy="guest-link"
+  >
+    Continue as a guest
+  </a>
 );
