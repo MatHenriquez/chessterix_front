f22316f2e49f6afc6afffe563b1b8aed0d4fddfa
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-01 20:52:55 -0300

    Add NotFound component for handling 404 errors

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div className="h-screen w-screen bg-[url(/image/404-not-found.png)] bg-cover bg-center bg-no-repeat" data-cy="not-found-page"></div>
+  );
+}
