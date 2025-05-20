c674827726053b5191029119dea515fb061ab20b
MatHenriquez <matias.henriquez.dev@gmail.com>
2025-05-19 23:18:51 -0300

    Add Ranks component and associated styles for displaying rank numbers

---
+++ app/board/components/Ranks.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import './ranks.css';
+
+type RanksProps = {
+  ranks: number[];
+};
+
+const Ranks: FC<RanksProps> = ({ ranks }) => {
+  return (
+    <div className="ranks bg-bone-main/10">
+      {ranks.map((rank) => (
+        <span key={rank} className="rank">
+          {rank}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default Ranks;
