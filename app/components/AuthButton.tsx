8b8eb3748d21ca6e565384c7e5c657fc7bba5ef6
MatHenriquez <matias.henriquez.dev@gmail.com>
2025-05-22 23:35:49 -0300

    Update SignInButton to use dynamic data-cy attribute with kebab case conversion

--- app/components/AuthButton.tsx
+++ app/components/AuthButton.tsx
@@ -1,9 +1,10 @@
+import { renameToKebabCase } from '@/utils/naming-conventions-converter';
 import React from 'react';
 
 const SignInButton = ({ showAuthModal, setShowAuthModal, text }: props) => (
   <button
     className="block w-full text-xl rounded lg:text-lg bg-red-main px-12 py-3 font-medium text-white shadow-md hover:bg-kimono-200 focus:outline-none ring-1 ring-white focus:ring-4 active:bg-kimono-700 sm:w-auto shadow-red-50/75"
-    data-cy="sign-in-button"
+    data-cy={renameToKebabCase(text + '-button')}
     onClick={() => setShowAuthModal(!showAuthModal)}
   >
     {text}
