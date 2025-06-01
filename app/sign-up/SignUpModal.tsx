7ad7185e135c8ba48739bf4089ff6c07b37eb7f6
Matías Henríquez <matias.henriquez.dev@gmail.com>
2025-06-01 18:49:21 -0300

    feat: refactor SignUpForm and SignUpModal for improved structure and usability

--- app/sign-up/SignUpModal.tsx
+++ app/sign-up/SignUpModal.tsx
@@ -1,41 +1,29 @@
 import React from 'react';
 import { SignUpForm } from './SignUpForm';
+import Modal from '@/components/Modal';
 
-const SignUpModal = ({ showSignUpModal, setShowSignUpModal }: props) => {
-  return (
-    <>
-      {showSignUpModal ? (
-        <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-kimono-900/90 outline-none focus:outline-none"
-              data-cy="sign-up-modal">
-                <button
-                  className="btn btn-lg text-xl btn-circle btn-ghost absolute right-2 top-2"
-                  onClick={() => setShowSignUpModal(!showSignUpModal)}
-                  data-cy="close-sign-up-modal"
-                >
-                  ✕
-                </button>
-                <div
-                  className="flex items-start justify-between p-5 border-blueGray-200 rounded-t w-screen"
-                  data-cy="sign-up-modal"
-                >
-                  <SignUpForm />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
-    </>
-  );
-};
-
-type props = {
+type Props = {
   showSignUpModal: boolean;
   setShowSignUpModal: (value: boolean) => void;
+};
+
+const SignUpModal = ({ showSignUpModal, setShowSignUpModal }: Props) => {
+  return (
+      <Modal
+        isOpen={showSignUpModal}
+        onClose={() => setShowSignUpModal(false)}
+        title={
+          <>
+            Go ahead...
+            <br />
+            <span className="text-lg">if you dare</span>
+          </>
+        }
+        dataCy="sign-up-modal"
+      >
+        <SignUpForm />
+      </Modal>
+  );
 };
 
 export default SignUpModal;
