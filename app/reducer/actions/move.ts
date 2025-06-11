c2c0ee8b962e0ccc5efcecfd9824f4c3e1290df0
MatHenriquez <matias.henriquez.dev@gmail.com>
2025-06-11 19:51:36 -0300

    feat: refactor action creators for candidate moves

--- app/reducer/actions/move.ts
+++ app/reducer/actions/move.ts
@@ -1,10 +1,28 @@
-import actionTypes from "../actionTypes";
+import actionTypes from '../actionTypes';
 
 export const makeNewMove = ({ newPosition }: { newPosition: string[][][] }) => {
   return {
     type: actionTypes.NEW_MOVE,
     payload: {
-      position: newPosition 
+      position: newPosition
     }
   };
 };
+
+export const generateCandidateMoves = (candidateMoves: number[][]) => {
+  return {
+    type: actionTypes.GENERATE_CANDIDATE_MOVES,
+    payload: {
+      candidateMoves
+    }
+  };
+};
+
+export const clearCandidateMoves = () => {
+  return {
+    type: actionTypes.CLEAR_CANDIDATE_MOVES,
+    payload: {
+      candidateMoves: []
+    }
+  };
+}
